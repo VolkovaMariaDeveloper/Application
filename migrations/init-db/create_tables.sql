7a468f7840822899d73ff38e8806ccd95ac70eb8
Volkova_Maria <volkovadeveloper@gmail.com>
2023-04-11 23:48:00 +0300

    fourth homework

---
+++ migrations/init-db/create_tables.sql
@@ -0,0 +1,25 @@
+--liquibase formatted sql
+
+--changeset Volcova:create_table_chat
+CREATE table IF NOT EXISTS chat
+(
+    chat_id INT PRIMARY KEY
+);
+
+--changeset Volcova:create_table_links
+CREATE table IF NOT EXISTS links
+(
+    url VARCHAR(255) NOT NULL,
+    linc_id INT PRIMARY KEY
+);
+
+--changeset svytoq:create_table_chat_link
+CREATE table IF NOT EXISTS chat_link
+(
+
+    chat_id INT,
+    link_id INT,
+    PRIMARY KEY (chat_id, link_id),
+    FOREIGN KEY (chat_id) REFERENCES chat(chat_id),
+    FOREIGN KEY (link_id) REFERENCES links(linc_id)
+);
