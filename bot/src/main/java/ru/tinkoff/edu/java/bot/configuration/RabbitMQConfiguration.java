640876f9b38f4803d57892890be2f2aaa4948cc8
Volkova_Maria <volkovadeveloper@gmail.com>
2023-05-04 10:51:35 +0300

    task 8.2

---
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/RabbitMQConfiguration.java
@@ -0,0 +1,52 @@
+package ru.tinkoff.edu.java.bot.configuration;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+
+public class RabbitMQConfiguration {
+    String exchangeName;
+    String queueName;
+
+    public RabbitMQConfiguration(String exchangeName, String queueName) {
+        this.exchangeName = exchangeName;
+        this.queueName = queueName;
+    }
+    @Bean
+    public CachingConnectionFactory connectionFactory(){
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
+        cachingConnectionFactory.setUsername("user");
+        cachingConnectionFactory.setPassword(("password"));
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange(exchangeName);
+    }
+    @Bean
+    public RabbitTemplate rabbitTemplate(){
+        return new RabbitTemplate(connectionFactory());
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName);
+    }
+
+    @Bean
+     Binding binding(DirectExchange directExchange, Queue queue) {
+        return BindingBuilder.bind(queue()).to(directExchange()).with("directRoutingKey");
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
